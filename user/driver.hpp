e06a52f1818b89c9fbade96bee49ce0405b0a995
Poko-Apps <parimals741@gmail.com>
2024-10-18 12:00:40 +0530

    user: handle r/w OP return types properly * rename struct based on driver code * rewrite MemKDriver cls for optimization

--- user/driver.hpp
+++ user/driver.hpp
@@ -1,118 +1,88 @@
 #include <sys/fcntl.h>
 #include <sys/ioctl.h>
+#include <unistd.h>
+#include <cstring>
+#include <cstdio>
 
 #define DEVICE_NAME "/dev/phmeop"
 
-class c_driver
-{
+class MemKDriver {
 private:
 	int fd;
 	pid_t pid;
 
-	typedef struct _COPY_MEMORY
+	struct CopyMemory
 	{
 		pid_t pid;
 		uintptr_t addr;
 		void *buffer;
 		size_t size;
-	} COPY_MEMORY, *PCOPY_MEMORY;
+	};
 
-	typedef struct _MODULE_BASE
+	struct ModuleBase
 	{
 		pid_t pid;
 		char *name;
 		uintptr_t base;
-	} MODULE_BASE, *PMODULE_BASE;
+	};
 
-	enum OPERATIONS
-	{
+	enum Operations {
 		OP_READ_MEM = 0x801,
 		OP_WRITE_MEM = 0x802,
 		OP_MODULE_BASE = 0x803,
 	};
 
 public:
-	c_driver()
-	{
-		fd = open(DEVICE_NAME, O_RDWR);
-		if (fd == -1)
-		{
-			printf("[-] open driver failed\n");
+	MemKDriver(const char* deviceName) : fd(open(deviceName, O_RDWR)) {
+		if (fd == -1) {
+			perror("[-] Failed to open driver");
 		}
 	}
 
-	~c_driver()
-	{
-		if (fd > 0)
+	~MemKDriver() {
+		if (fd > 0) {
 			close(fd);
-	}
-
-	void initialize(pid_t pid)
-	{
-		this->pid = pid;
-	}
-
-	bool read(uintptr_t addr, void *buffer, size_t size)
-	{
-		COPY_MEMORY cm;
-
-		cm.pid = this->pid;
-		cm.addr = addr;
-		cm.buffer = buffer;
-		cm.size = size;
-
-		if (ioctl(fd, OP_READ_MEM, &cm) != 0)
-		{
-			return false;
 		}
-		return true;
 	}
 
-	bool write(uintptr_t addr, void *buffer, size_t size)
-	{
-		COPY_MEMORY cm;
+	inline void initialize(const pid_t new_pid) {
+		pid = new_pid;
+	}
 
-		cm.pid = this->pid;
-		cm.addr = addr;
-		cm.buffer = buffer;
-		cm.size = size;
+	ssize_t read(const uintptr_t addr, void *buffer, const size_t size) const {
+		if (!buffer || fd == -1) return -1;
 
-		if (ioctl(fd, OP_WRITE_MEM, &cm) != 0)
-		{
-			return false;
-		}
-		return true;
+		CopyMemory cm = { pid, addr, buffer, size };
+		return ioctl(fd, OP_READ_MEM, &cm);
+	}
+
+	ssize_t write(const uintptr_t addr, const void *buffer, const size_t size) const {
+		if (!buffer || fd == -1) return -1;
+
+		CopyMemory cm = { pid, addr, const_cast<void *>(buffer), size };
+		return ioctl(fd, OP_WRITE_MEM, &cm);
 	}
 
 	template <typename T>
-	T read(uintptr_t addr)
-	{
-		T res;
-		if (this->read(addr, &res, sizeof(T)))
-			return res;
-		return {};
+	inline T read(const uintptr_t addr) const {
+		T result{};
+		read(addr, &result, sizeof(T));
+		return result;
 	}
 
 	template <typename T>
-	bool write(uintptr_t addr, T value)
-	{
-		return this->write(addr, &value, sizeof(T));
+	inline bool write(const uintptr_t addr, const T &value) const {
+		return write(addr, &value, sizeof(T)) == sizeof(T);
 	}
 
-	uintptr_t get_module_base(char *name)
-	{
-		MODULE_BASE mb;
-		char buf[0x100];
-		strcpy(buf, name);
-		mb.pid = this->pid;
-		mb.name = buf;
+	inline uintptr_t get_module_base(const char *name) const {
+		if (!name || fd == -1) return 0;
 
-		if (ioctl(fd, OP_MODULE_BASE, &mb) != 0)
-		{
-			return 0;
-		}
-		return mb.base;
+		ModuleBase mb = { pid, nullptr, 0 };
+		mb.name = const_cast<char *>(name);
+
+		return (ioctl(fd, OP_MODULE_BASE, &mb) == 0) ? mb.base : 0;
 	}
 };
 
-static c_driver *driver = new c_driver();
+static MemKDriver *driver = new MemKDriver(DEVICE_NAME);
